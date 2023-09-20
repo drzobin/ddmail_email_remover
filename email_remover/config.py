95dc9792e1c7d64a3bfa362480d8090520279696
drzobin <me@drz.se>
2023-09-20 15:45:36 +0200

    Add vars data for test and dev

--- email_remover/config.py
+++ email_remover/config.py
@@ -5,10 +5,10 @@ class Prod():
 
 class Test():
     SECRET_KEY = 'change_me'
-    PASSWORD_HASH = 'change_me'
-    EMAIL_ACCOUNT_PATH = '/var/mail/vhosts/'
+    PASSWORD_HASH = '$argon2id$v=19$m=65536,t=3,p=4$mzqiveNU7Y9jwj6I9cadUw$hVabWZSN+fPRzBqDrEMOSCfOQEYgq8mRzc2ny7azCXA'
+    EMAIL_ACCOUNT_PATH = '/tmp/mail/vhost/'
 
 class Dev():
     SECRET_KEY = 'change_me'
-    PASSWORD_HASH = 'change_me'
-    EMAIL_ACCOUNT_PATH = '/var/mail/vhosts/'
+    PASSWORD_HASH = '$argon2id$v=19$m=65536,t=3,p=4$mzqiveNU7Y9jwj6I9cadUw$hVabWZSN+fPRzBqDrEMOSCfOQEYgq8mRzc2ny7azCXA'
+    EMAIL_ACCOUNT_PATH = '/tmp/mail/vhost/'
