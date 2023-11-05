af85f2632af8b1c78052c49ceb34672d86b9e75d
drz <me@drz.se>
2023-11-05 12:59:42 +0100

    Add testcases

--- email_remover/tests/test_application.py
+++ email_remover/tests/test_application.py
@@ -7,44 +7,32 @@ def test_hash_data(client):
     assert response_hash_data_post.status_code == 200
     assert b"argon2id" in response_hash_data_post.data
 
-def test_main(client,app):
-    response_main_post = client.post("/", data={"password":"password", "domain":"test.se", "email":"test@test.se"})
-    assert response_main_post.status_code == 200
-    assert b"done" in response_main_post.data
-
-    with app.app_context():
-        test_email_folder  = current_app.config["EMAIL_ACCOUNT_PATH"] + "/test.se" + "/test"
-        assert os.path.exists(test_email_folder) == False
+def test_hash_data_illigal_char(client):
+    response_hash_data_post = client.post("/hash_data", data={"data":"passwor..d"})
+    assert response_hash_data_post.status_code == 200
+    assert b"error: password validation failed" in response_hash_data_post.data
 
 def test_main_wrong_password(client,app):
     response_main_post = client.post("/", data={"password":"wrongpassword", "domain":"test.se", "email":"test@test.se"})
     assert response_main_post.status_code == 200
     assert b"error: wrong password" in response_main_post.data
 
-    with app.app_context():
-        test_email_folder  = current_app.config["EMAIL_ACCOUNT_PATH"] + "/test.se" + "/test"
-        assert os.path.exists(test_email_folder) == True
-
 def test_main_illigal_char_email(client,app):
     response_main_post = client.post("/", data={"password":"password", "domain":"test.se", "email":"test@test..se"})
     assert response_main_post.status_code == 200
     assert b"error: email validation failed" in response_main_post.data
-
-    with app.app_context():
-        test_email_folder  = current_app.config["EMAIL_ACCOUNT_PATH"] + "/test.se" + "/test"
 
 def test_main_illigal_char_password(client,app):
     response_main_post = client.post("/", data={"password":"..password", "domain":"test.se", "email":"test@test.se"})
     assert response_main_post.status_code == 200
     assert b"error: password validation failed" in response_main_post.data
 
-    with app.app_context():
-        test_email_folder  = current_app.config["EMAIL_ACCOUNT_PATH"] + "/test.se" + "/test"
-
 def test_main_illigal_char_domain(client,app):
     response_main_post = client.post("/", data={"password":"password", "domain":"..test.se", "email":"test@test.se"})
     assert response_main_post.status_code == 200
     assert b"error: domain validation failed" in response_main_post.data
 
-    with app.app_context():
-        test_email_folder  = current_app.config["EMAIL_ACCOUNT_PATH"] + "/test.se" + "/test"
+def test_main_not_matching_domain(client,app):
+    response_main_post = client.post("/", data={"password":"password", "domain":"test.se", "email":"test@testtest.se"})
+    assert response_main_post.status_code == 200
+    assert b"error: email adress domain do not match domain" in response_main_post.data
