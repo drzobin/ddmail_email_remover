f800b9c8bfebdacab990df925bd28ecc6c8057ee
drzobin <me@drz.se>
2025-03-12 16:27:43 +0100

    Remove unused hash for test and dev

--- email_remover/config.py
+++ email_remover/config.py
@@ -5,10 +5,10 @@ class Prod():
 
 class Test():
     SECRET_KEY = 'change_me'
-    PASSWORD_HASH = '$argon2id$v=19$m=65536,t=3,p=4$mzqiveNU7Y9jwj6I9cadUw$hVabWZSN+fPRzBqDrEMOSCfOQEYgq8mRzc2ny7azCXA'
+    PASSWORD_HASH = 'change_me'
     EMAIL_ACCOUNT_PATH = '/tmp/mail/vhost/'
 
 class Dev():
     SECRET_KEY = 'change_me'
-    PASSWORD_HASH = '$argon2id$v=19$m=65536,t=3,p=4$mzqiveNU7Y9jwj6I9cadUw$hVabWZSN+fPRzBqDrEMOSCfOQEYgq8mRzc2ny7azCXA'
+    PASSWORD_HASH = 'change_me'
     EMAIL_ACCOUNT_PATH = '/tmp/mail/vhost/'
