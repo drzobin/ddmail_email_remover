850dac0338712e28f05995d96635e89643af3922
drzobin <me@drz.se>
2025-03-12 18:56:14 +0100

    Refactor to us toml config

--- src/ddmail_email_remover/__init__.py
+++ src/ddmail_email_remover/__init__.py
@@ -1,23 +1,42 @@
 import os
+import toml
 from flask import Flask
 
 
-def create_app(test_config=None):
-    """Create and configure an instance of the Flask application email_remover."""
+def create_app(config_file = None, test_config = None):
+    """Create and configure an instance of the Flask application ddmail_email_remover."""
     app = Flask(__name__, instance_relative_config=True)
 
-    # Set app configurations from configuration file config.py
+    toml_config = None
+
+    # Check if config_file has been set.
+    if config_file is None:
+        print("Error: you need to set path to configuration file in toml format")
+        sys.exit(1)
+
+    # Parse toml config file.
+    with open(config_file, 'r') as f:
+        toml_config = toml.load(f)
+
+    # Set app configurations from toml config file.
     mode=os.environ.get('MODE')
+    print("Running in MODE: " + mode)
     if mode == "PRODUCTION":
-        app.config.from_object("config.Prod")
+        app.config["SECRET_KEY"] = toml_config["PRODUCTION"]["SECRET_KEY"]
+        app.config["PASSWORD_HASH"] = toml_config["PRODUCTION"]["PASSWORD_HASH"]
+        app.config["EMAIL_ACCOUNT_PATH"] = toml_config["PRODUCTION"]["EMAIL_ACCOUNT_PATH"]
     elif mode == "TESTING":
-        app.config.from_object("config.Test")
+        app.config["SECRET_KEY"] = toml_config["TESTING"]["SECRET_KEY"]
+        app.config["PASSWORD_HASH"] = toml_config["TESTING"]["PASSWORD_HASH"]
+        app.config["EMAIL_ACCOUNT_PATH"] = toml_config["TESTING"]["EMAIL_ACCOUNT_PATH"]
     elif mode == "DEVELOPMENT":
-        app.config.from_object("config.Dev")
+        app.config["SECRET_KEY"] = toml_config["DEVELOPMENT"]["SECRET_KEY"]
+        app.config["PASSWORD_HASH"] = toml_config["DEVELOPMENT"]["PASSWORD_HASH"]
+        app.config["EMAIL_ACCOUNT_PATH"] = toml_config["DEVELOPMENT"]["EMAIL_ACCOUNT_PATH"]
     else:
         print("Error: you need to set env variabel MODE to PRODUCTION/TESTING/DEVELOPMENT")
-        exit(1)
-    
+        sys.exit(1)
+
     app.secret_key = app.config["SECRET_KEY"]
 
     # Ensure the instance folder exists
@@ -27,7 +46,7 @@ def create_app(test_config=None):
         pass
 
     # Apply the blueprints to the app
-    from email_remover import application
+    from ddmail_email_remover import application
     app.register_blueprint(application.bp)
 
     return app 
