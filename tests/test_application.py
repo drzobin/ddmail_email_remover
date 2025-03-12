50bbbf46ffcc32462ee58459c49f7a0c4fecfdb2
drzobin <me@drz.se>
2025-03-12 19:00:45 +0100

    Remove test for removed func

--- tests/test_application.py
+++ tests/test_application.py
@@ -2,16 +2,6 @@ from flask import current_app
 import pytest
 import os
 
-def test_hash_data(client):
-    response_hash_data_post = client.post("/hash_data", data={"data":"password"})
-    assert response_hash_data_post.status_code == 200
-    assert b"argon2id" in response_hash_data_post.data
-
-def test_hash_data_illigal_char(client):
-    response_hash_data_post = client.post("/hash_data", data={"data":"passwor..d"})
-    assert response_hash_data_post.status_code == 200
-    assert b"error: password validation failed" in response_hash_data_post.data
-
 def test_main_wrong_password(client,app):
     response_main_post = client.post("/", data={"password":"wrongpassword", "domain":"test.se", "email":"test@test.se"})
     assert response_main_post.status_code == 200
