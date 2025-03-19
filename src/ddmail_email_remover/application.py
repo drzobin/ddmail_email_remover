510ff15954d0e08de683cf0213c3306a2593789b
drzobin <me@drz.se>
2025-03-19 08:37:27 +0100

    Fix logger problem

--- src/ddmail_email_remover/application.py
+++ src/ddmail_email_remover/application.py
@@ -20,35 +20,35 @@ def main():
 
         # Validate password.
         if validators.is_password_allowed(password) != True:
-            app.logger.error("main() password validation failed")
+            current_app.logger.error("password validation failed")
             return "error: password validation failed"
 
         # Validate domain.
         if validators.is_domain_allowed(domain) != True:
-            app.logger.error("main() domain validation failed")
+            current_app.logger.error("domain validation failed")
             return "error: domain validation failed"
 
         # Validate email.
         if validators.is_email_allowed(email) != True:
-            app.logger.error("main() domain validation failed")
+            current_app.logger.error("domain validation failed")
             return "error: email validation failed"
 
         # Check if password is correct.
         try:
             if ph.verify(current_app.config["PASSWORD_HASH"], password) != True:
                 time.sleep(1)
-                app.logger.error("main() wrong password")
+                current_app.logger.error("wrong password")
                 return "error: wrong password"
         except VerifyMismatchError:
             time.sleep(1)
-            app.logger.error("main() exceptions VerifyMismatchError, wrong password")
+            current_app.logger.error("exceptions VerifyMismatchError, wrong password")
             return "error: wrong password"
         time.sleep(1)
 
         # Split email and verify domain.
         splitted_email_domain = email.split('@')
         if splitted_email_domain[1] != domain:
-            app.logger.error("main() email address domain do no match domain")
+            current_app.logger.error("email address domain do no match domain")
             return "error: email adress domain do not match domain"
 
         # Path to email folder on disc.
@@ -59,21 +59,21 @@ def main():
 
         # Check that rm exist.
         if os.path.exists(rm) != True:
-            app.logger.error("main() rm binary location is wrong")
+            current_app.logger.error("rm binary location is wrong")
             return "error: rm binary location is wrong"
 
         # Remove email folder from hdd.
         try:
             output = subprocess.run(["/usr/bin/doas","-u","vmail",rm,"-rf",email_path], check=True)
             if output.returncode != 0:
-                app.logger.error("main() returncode of cmd rm is non zero")
+                current_app.logger.error("returncode of cmd rm is non zero")
                 return "error: returncode of cmd rm is non zero"
         except subprocess.CalledProcessError as e:
-            app.logger.error("main() returncode of cmd rm is non zero")
+            current_app.logger.error("returncode of cmd rm is non zero")
             return "error: returncode of cmd rm is non zero"
         except:
-            app.logger.error("main() unkonwn exception running subprocess")
+            current_app.logger.error("unkonwn exception running subprocess")
             return "error: unkonwn exception running subprocess"
 
-        app.logger.info("main() done")
+        current_app.logger.info("done")
         return "done"
