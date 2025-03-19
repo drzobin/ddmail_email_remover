ddc04a1bd83a15d9a3a16cd6eb728f3d6e0569fd
drzobin <me@drz.se>
2025-03-19 07:38:38 +0100

    Change to use app.logger for logging

--- src/ddmail_email_remover/application.py
+++ src/ddmail_email_remover/application.py
@@ -2,16 +2,12 @@ import os
 import shutil
 import time
 import subprocess
-import logging
 from flask import Blueprint, current_app, request
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 import ddmail_validators.validators as validators
 
 bp = Blueprint("application", __name__, url_prefix="/")
-
-# Configure logging.
-logging.basicConfig(filename="/var/log/ddmail_email_remover.log", format='%(asctime)s: %(levelname)s: %(message)s', level=logging.ERROR)
 
 @bp.route("/", methods=["POST"])
 def main():
@@ -24,35 +20,35 @@ def main():
 
         # Validate password.
         if validators.is_password_allowed(password) != True:
-            logging.error("main() password validation failed")
+            app.logger.error("main() password validation failed")
             return "error: password validation failed"
 
         # Validate domain.
         if validators.is_domain_allowed(domain) != True:
-            logging.error("main() domain validation failed")
+            app.logger.error("main() domain validation failed")
             return "error: domain validation failed"
 
         # Validate email.
         if validators.is_email_allowed(email) != True:
-            logging.error("main() domain validation failed")
+            app.logger.error("main() domain validation failed")
             return "error: email validation failed"
 
         # Check if password is correct.
         try:
             if ph.verify(current_app.config["PASSWORD_HASH"], password) != True:
                 time.sleep(1)
-                logging.error("main() wrong password")
+                app.logger.error("main() wrong password")
                 return "error: wrong password"
         except VerifyMismatchError:
             time.sleep(1)
-            logging.error("main() exceptions VerifyMismatchError, wrong password")
+            app.logger.error("main() exceptions VerifyMismatchError, wrong password")
             return "error: wrong password"
         time.sleep(1)
 
         # Split email and verify domain.
         splitted_email_domain = email.split('@')
         if splitted_email_domain[1] != domain:
-            logging.error("main() email address domain do no match domain")
+            app.logger.error("main() email address domain do no match domain")
             return "error: email adress domain do not match domain"
 
         # Path to email folder on disc.
@@ -63,21 +59,21 @@ def main():
 
         # Check that rm exist.
         if os.path.exists(rm) != True:
-            logging.error("main() rm binary location is wrong")
+            app.logger.error("main() rm binary location is wrong")
             return "error: rm binary location is wrong"
 
         # Remove email folder from hdd.
         try:
             output = subprocess.run(["/usr/bin/doas","-u","vmail",rm,"-rf",email_path], check=True)
             if output.returncode != 0:
-                logging.error("main() returncode of cmd rm is non zero")
+                app.logger.error("main() returncode of cmd rm is non zero")
                 return "error: returncode of cmd rm is non zero"
         except subprocess.CalledProcessError as e:
-            logging.error("main() returncode of cmd rm is non zero")
+            app.logger.error("main() returncode of cmd rm is non zero")
             return "error: returncode of cmd rm is non zero"
         except:
-            logging.error("main() unkonwn exception running subprocess")
+            app.logger.error("main() unkonwn exception running subprocess")
             return "error: unkonwn exception running subprocess"
 
-        logging.debug("main() done")
+        app.logger.info("main() done")
         return "done"
